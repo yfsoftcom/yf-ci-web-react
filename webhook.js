5b500ee92513b40dc05db16f27c7f643db6092b5
yfsoftcom <yfsoftcom@126.com>
2017-02-04 23:16:01 +0800

    add webhook

--- webhook.js
+++ webhook.js
@@ -2,6 +2,7 @@ import runCommand from './command.js'
 
 const reps = {
   'yf-ci-web-react': 'www.xinyangjlm.com',
+  '':
 }
 export default async function ( upstream, args ){
   return new Promise((resolve, reject) => {
@@ -9,7 +10,11 @@ export default async function ( upstream, args ){
       case 'github':
         let repName = args.repository.name;
         runCommand('yfcode pull ' + reps[repName])
-        resolve('success');
+        resolve('success')
+        break;
+      case 'coding':
+        console.log(args)
+        resolve('test')
         break;
       default:
         resolve('unknow upstream')
