d6dd79636426f5c22d949d8f5b10ebc18f89c5e7
yfsoftcom <yfsoftcom@126.com>
2017-02-05 11:18:42 +0800

    add params

--- webhook.js
+++ webhook.js
@@ -7,10 +7,10 @@ export default async function ( params, args ){
   let upstream = params.upstream
   switch(upstream){
     case 'github':
-      result = await runCommand('yfcode pull ' + type + ' ' + project)
+      result = await runCommand('yfcode pull -' + type + ' ' + project)
       break;
     case 'coding':
-      result = await runCommand('yfcode pull ' + type + ' ' + project)
+      result = await runCommand('yfcode pull -' + type + ' ' + project)
       break;
     default:
       result = 'unknow upstream'
